a1a052da4d04964169b159f3dc11c2a11a5d77b0
dychel <dycheloko20@gmail.com>
2025-01-04 00:14:07 +0100

    job activities

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JobListing from '../components/JobListing'
+import JobListings from '../components/JobListings'
+const JobsPage = () => {
+  return (
+    <section className='bg-blue px-4 py-6'>
+        <JobListings />
+    </section>
+  )
+}
+
+export default JobsPage
